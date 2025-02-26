5d27aa805485f57662a6d5677fc73f68fced5b27
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-02-25 22:11:12 -0500

    feat: add topbar in Editor

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -5,11 +5,13 @@ import React from 'react'
 import { ReactFlowProvider } from '@xyflow/react'
 import FlowEditor from './FlowEditor'
 import { WorkflowFactoryData } from '@/core/domain/workflow/workflow.factory'
+import TopBar from './topbar/TopBar'
 
 function Editor({ workflow }: { workflow: WorkflowFactoryData }) {
   return (
     <ReactFlowProvider>
       <div className='flex flex-col h-full w-full overflow-hidden'>
+        <TopBar title='Workflow editor' subtitle={workflow.name} />
         <section className='flex h-full overflow-auto'>
           <FlowEditor workflow={workflow} />
         </section>
---
+++ app/workflow/_components/topbar/TopBar.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import React from 'react'
+import TooltipWrapper from '@/ui/components/TooltipWrapper'
+import { Button } from '@/ui/components/ui/button'
+import { ChevronLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+type TopBarProps = {
+    title: string;
+    subtitle?: string;
+}
+
+function TopBar({ title, subtitle }: TopBarProps) {
+    const router = useRouter();
+
+    return (
+        <header className='flex p-2 border-p-2 border-separate justify-between w-full h-[60px] sticky top-0 bg-background z-10'>
+            <div className='flex gap-1 flex-1'>
+                <TooltipWrapper content='Back'>
+                    <Button
+                        variant='ghost'
+                        size='icon'
+                        onClick={() => router.push('/workflows')}
+                    >
+                        <ChevronLeft size={20} />
+                    </Button>
+                </TooltipWrapper>
+                <div>
+                    <p className='font-bold text-ellipsis truncate'>
+                        {title}
+                    </p>
+                    {subtitle && (
+                        <p className='text-xs text-muted-foreground truncate text-ellipsis'>
+                            {subtitle}
+                        </p>
+                    )}
+                </div>
+            </div>
+            <div className='flex gap-1 flex-1 justify-end'>
+                {/* TODO: Add save button here */}
+            </div>
+        </header>
+    );
+}
+
+export default TopBar
