6442edf9170f6c15952b869dd5edf0c7bef53125
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-02-26 21:41:54 -0500

    feat: disable save button when saving

--- app/workflow/_components/topbar/SaveButton.tsx
+++ app/workflow/_components/topbar/SaveButton.tsx
@@ -23,6 +23,7 @@ function SaveButton({ workflowId }: { workflowId: string }) {
 
     return (
         <Button
+            disabled={saveMutation.isPending}
             variant='outline'
             className='flex items-center gap-2'
             onClick={() => {
