b72eca64d305b146642ae1e2644807dbd696101b
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-02-28 16:19:34 -0500

    feat: add delete and duplicate buttons on task in editor

--- app/workflow/_components/nodes/NodeHeader.tsx
+++ app/workflow/_components/nodes/NodeHeader.tsx
@@ -1,14 +1,19 @@
 'use client'
 
+import { createFlowNode } from '@/lib/workflow/createFlowNode';
 import { TaskRegistry } from '@/lib/workflow/task/registry'
 import { Badge } from '@/ui/components/ui/badge';
 import { Button } from '@/ui/components/ui/button';
+import { AppNode } from '@/ui/types/app-node';
 import { TaskType } from '@/ui/types/task'
-import { CoinsIcon, GripVerticalIcon } from 'lucide-react';
+import { useReactFlow } from '@xyflow/react';
+import { CoinsIcon, CopyIcon, GripVerticalIcon, TrashIcon } from 'lucide-react';
 import React from 'react'
 
-function NodeHeader({ taskType }: { taskType: TaskType }) {
+function NodeHeader({ taskType, nodeId }: { taskType: TaskType, nodeId: string }) {
     const task = TaskRegistry[taskType];
+    const { deleteElements, getNode, addNodes } = useReactFlow();
+
     return (
         <div className='flex items-center gap-2 p-2'>
             <task.icon size={16} />
@@ -22,6 +27,32 @@ function NodeHeader({ taskType }: { taskType: TaskType }) {
                         <CoinsIcon size={16} />
                         10
                     </Badge>
+                    {!task.isEntryPoint &&
+                        <>
+                            <Button
+                                variant='ghost'
+                                size='icon'
+                                onClick={() => deleteElements({
+                                    nodes: [{ id: nodeId }]
+                                })}
+                            >
+                                <TrashIcon size={12} />
+                            </Button>
+                            <Button
+                                variant='ghost'
+                                size='icon'
+                                onClick={() => {
+                                    const node = getNode(nodeId) as AppNode;
+                                    const newX = node.position.x;
+                                    const newY = node.position.y + node.measured?.height! + 20;
+                                    const newNode = createFlowNode(node.data.type, { x: newX, y: newY });
+                                    addNodes([newNode])
+                                }}
+                            >
+                                <CopyIcon size={12} />
+                            </Button>
+                        </>
+                    }
                     <Button variant='ghost' size='icon' className='drag-handle cursor-grab'>
                         <GripVerticalIcon size={20} />
                     </Button>
