a3ff9406439390ffc5fe44889f7697b9238626ea
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-03-13 22:51:53 -0400

    chore: correct wallaby.js configuration

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,34 +1,20 @@
 module.exports = function (wallaby) {
   return {
+    autoDetect: true,
+
     files: [
       'core/**/*.ts',
-      'core/**/*.tsx',
-      'core/**/*.js',
-      'core/**/*.jsx',
-      '!core/**/*.test.ts',
-      '!core/**/*.test.tsx',
-      '!core/**/*.test.js',
-      '!core/**/*.test.jsx'
+      'lib/**/*.ts',
+      'infra/**/*.ts',
+      'jest.setup.ts',
+      'package.json',
+      'tsconfig.json',
+      'jest.config.ts',
+      '!core/**/*.test.ts'
     ],
 
     tests: [
-      'core/**/*.test.ts',
-      'core/**/**/*.test.ts',
-      'core/**/*.test.tsx',
-      'core/**/*.test.js',
-      'core/**/*.test.jsx'
+      'core/**/*.test.ts'
     ],
-
-    env: {
-      type: 'node',
-      runner: 'node'
-    },
-
-    testFramework: 'jest',
-
-    setup: function (wallaby) {
-      const jestConfig = require('./jest.config.ts');
-      wallaby.testFramework.configure(jestConfig);
-    }
   };
 };
