b8317fc85b8de2b30c3c1fd078ca3aa8e3167651
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-03-13 22:53:09 -0400

    chore: simplify wallaby.js configuration

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,18 +1,6 @@
 module.exports = function (wallaby) {
   return {
     autoDetect: true,
-
-    files: [
-      'core/**/*.ts',
-      'lib/**/*.ts',
-      'infra/**/*.ts',
-      'jest.setup.ts',
-      'package.json',
-      'tsconfig.json',
-      'jest.config.ts',
-      '!core/**/*.test.ts'
-    ],
-
     tests: [
       'core/**/*.test.ts'
     ],
