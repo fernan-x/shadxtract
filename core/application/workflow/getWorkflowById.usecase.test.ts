8c55bfa667ed27d449342c8b15bb6a72e29d160b
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-03-16 12:37:06 -0400

    test: add GetWorkflowById usecase unit tests

---
+++ core/application/workflow/getWorkflowById.usecase.test.ts
@@ -0,0 +1,19 @@
+import { WorkflowRepository } from '@/core/domain/workflow/workflow.repository';
+import { WorkflowRepositoryInMemory } from '@/infra/repositories/workflow/workflow.repository.in-memory';
+import { GetWorkflowByIdUseCase } from './getWorkflowById.usecase';
+
+describe('USECASE: GetWorkflowByIdUseCase', () => {
+    let useCase: GetWorkflowByIdUseCase;
+    let inMemoryRepository: WorkflowRepository;
+
+    beforeEach(() => {
+        inMemoryRepository = new WorkflowRepositoryInMemory();
+        useCase = new GetWorkflowByIdUseCase(inMemoryRepository);
+    });
+
+    it('should get a workflow by id', async () => {
+        const workflow = await inMemoryRepository.get('1');
+        expect(workflow).toBeDefined();
+        expect(workflow?.id).toBe('1');
+    });
+});
